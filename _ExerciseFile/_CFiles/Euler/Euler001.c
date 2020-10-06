82cf8753ab2399600dfc86bf3f32605bf98f623d
kani-devbox <kani-devbox@deb10.devbox.pt>
2020-10-06 01:09:03 -0400

    update

--- _ExerciseFile/_CFiles/Euler/Euler001.c
+++ _ExerciseFile/_CFiles/Euler/Euler001.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
-using namespace std;
-/* https://projecteuler.net/problem=1 */
 
-
-int main(int argc, char** argv)
+int sum_ck_0_to_n(int c, int n)
 {
-    unsigned int sum = 0;
-    for(int i = 1; i < 1001; ++i)
-        if(!(i % 3) && !(i % 5))
-            sum += i;
-    std::cout << i;
-    return 0;
+	/* SUM(k){k=0...n} = n(n+1)/2 */
+	/* SUM(ck){k=0...n} = cn(n+1)/2 */
+	return (c*n*(n+1))/2;
+}
+
+int main(int argc, char *argv[])
+{
+	int sum = sum_ck_0_to_n(3,999/3) + sum_ck_0_to_n(5,999/5);
+	/* The least common multiple was included twice, remove 1 */
+	sum -= sum_ck_0_to_n(15,999/15);
+	printf("Problem1 = %i\n", sum);
+	return 0;
 }
