6e88e96ac92afbd1b3d423f8bb759cb230a06b27
kani-devbox <kani-devbox@deb10.devbox.pt>
2020-10-07 20:05:39 -0400

    reordered the folders

--- _ExerciseFile/Python_Exercises/WebApp/flaskSite.py
+++ _ExerciseFile/Python_Exercises/WebApp/flaskSite.py
@@ -1,11 +1,11 @@
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-
 def home():
-    return "Website content goes here!"
+    return "Welcome to my website"
 
 if __name__ == "__main__":
     app.run(debug=True)
